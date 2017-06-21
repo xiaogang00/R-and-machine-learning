f3fef99aece84e6a3fe08d36408d7bcf98d7d582
xiaogang00 <872310734@qq.com>
2017-06-21 08:14:35 +0800

    data mining code based on R

---
+++ R语言数据分析与挖掘实战/第十一章/11-5_arima_model_check.R
@@ -0,0 +1,17 @@
+setwd("F:/数据及程序/chapter11/示例程序")
+library(forecast)
+Data <- read.csv("./data/discdata_processed.csv", header = TRUE)
+colnames(Data) <- c("SYS_NAME", "COLLECTTIME", "CWC", "CWD")
+attach(Data)
+m1 <- arima(CWD, order = c(0, 1, 1))
+r1 <- m1$residuals 
+# 对残差进行平稳性检验
+adfTest(r1)
+# 对残差进行随机性检验
+Box.test(r1, type = "Ljung-Box") 
+m2 <- arima(CWD, order = c(0, 1, 2))
+r2 <- m1$residuals
+# 对残差进行平稳性检验
+adfTest(r2)
+# 对残差进行纯随机性检验
+Box.test(r2, type = "Ljung-Box")
---
+++ R语言数据分析与挖掘实战/第十一章/11-6_cal_errors.R
@@ -0,0 +1,10 @@
+setwd("F:/数据及程序/chapter11/示例程序")
+Data <- read.csv("./data/predictdata.csv", header = TRUE)
+colnames(Data) <- c("num", "pre", "real")
+attach(Data)
+# 平均绝对误差
+mae <- mean(abs(pre - real))
+# 均方根误差
+rmse <- mean((pre - real) ^ 2)
+# 平均绝对百分误差
+mape <- mean(abs(pre - real) / real)
